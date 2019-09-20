d898a75607ad6e2496ea0952723b708047c2f544
ruanjunpeng <junpengruan@outlook.com>
2019-09-20 16:16:27 +0800

    Add files via upload

--- Sample/src/readcounts2TPM.R
+++ Sample/src/readcounts2TPM.R
@@ -4,9 +4,9 @@ library(rjson)
 json_data<-fromJSON(file="Tool.config")
 
 gene_length_file = json_data$config$transcriptome$gene_length_file
-ROSMAP_count_file = json_data$config$transcriptome$readcounts
+Count_file = json_data$config$transcriptome$readcounts
 
-#ROSMAP_count_file = "C:/Users/rjp/Desktop/TEST/GD660.GeneQuantCount.txt"
+#Count_file = "./GD660.GeneQuantCount.txt"
 #gene_length_file = "C:/Data/gencode.v24.annotation.gtf.gene_length_unionexion.txt"
 
 library(tidyverse)
@@ -40,21 +40,22 @@ names(gene_length) = c("ENSGID", "length")
 # remove gene name version
 gene_length$ENSGID= do.call(rbind, strsplit(gene_length$ENSGID, '.', fixed=T))[,1]
 
-#ROSMAP_count_file = "C:/Data/ROSMAP_all_counts_matrix.txt"
-ROSMAP_count = read.table(ROSMAP_count_file, header=T, sep = "\t", stringsAsFactors=F, check.names=F)
-#ROSMAP_count = ROSMAP_count[-c(1:4),]
-ROSMAP_count = ROSMAP_count[,-c(2:4)]
-ROSMAP_count$TargetID = do.call(rbind, strsplit(ROSMAP_count$TargetID, '.', fixed=T))[,1]
-comm = intersect(gene_length$ENSGID, ROSMAP_count$TargetID)
 
-rownames(ROSMAP_count) = ROSMAP_count$TargetID; ROSMAP_count = ROSMAP_count[,-1]
+Count = read.table(Count_file, header=T, sep = "\t", stringsAsFactors=F, check.names=F)
+#user should pre-format the read count and delete extra rows and cols
+#Count = Count[-c(1:4),]
+#Count = Count[,-c(2:4)]
+Count$TargetID = do.call(rbind, strsplit(Count$TargetID, '.', fixed=T))[,1]
+comm = intersect(gene_length$ENSGID, Count$TargetID)
+
+rownames(Count) = Count$TargetID; Count = Count[,-1]
 rownames(gene_length) = gene_length$ENSGID; 
-ROSMAP_count = ROSMAP_count[comm,]
+Count = Count[comm,]
 gene_length = gene_length[comm,]
-ROSMAP_tpm = counts_to_tpm(ROSMAP_count, gene_length$length)
+tpm_data = counts_to_tpm(Count, gene_length$length)
 
 
 
 
-write.csv(ROSMAP_tpm,"TPM_matrix.csv")
+write.csv(tpm_data,"TPM_matrix.csv")
 
